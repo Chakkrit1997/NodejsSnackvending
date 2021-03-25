9b6e1126cac624ce21632a83b74140665920fe2a
Ourlandx <41711618+Ourlandx@users.noreply.github.com>
2021-03-25 16:04:57 +0700

    input file but note upload

--- routes/overviews.js
+++ routes/overviews.js
@@ -1,12 +1,21 @@
 module.exports = {
     Overviews: (req, res) => {
-        var amount;
-        var ref = database.ref("nowsnack/s0").on('value',function(snapshot){
-            amount = snapshot.val().amount;
-        });
+        // var amount;
+        // var ref = database.ref("nowsnack/s0").on('value',function(snapshot){
+        //     amount = snapshot.val().amount;
+        // });
 
-        res.render('pages/overviews.ejs', {
-            title: "Welcome to snack vending"
-        });
+        // res.render('pages/overviews.ejs', {
+        //     title: "Welcome to snack vending"
+        // });
+
+        var snackobj = database.ref("nowsnack").once('value').then(function (dataSnapshot) {
+            obj = dataSnapshot.val();
+            res.render('pages/overviews.ejs', {
+                title: "Welcome to snack vending",
+                value: obj,
+            });
+        })
     }
+    
 }
